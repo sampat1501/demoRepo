63ce0d93bb8880ecee33b147f9c369da7785aaf2
sampat <bsampat0505@gmail.com>
2023-01-02 21:39:14 +0530

    Commit

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo
+{
+public static void main(String [] args)
+{
+System.out.println("Sampat demo print");
+}
+}
---
+++ Test1.java
@@ -0,0 +1,8 @@
+Class Test1{
+
+public static void main( String[] args)
+{
+
+System.out.println("Nikhil is Clevar boy");
+}
+}
