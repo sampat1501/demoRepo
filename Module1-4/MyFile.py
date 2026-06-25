4a93301f2e203ac3c876cdddbf7d07f33d8623e3
Sampat Bansode <sampat.bansode@agiliad.local>
2026-06-25 17:58:27 +0530

    module6 code

--- Module1-4/MyFile.py
+++
@@ -1,4 +0,0 @@
-a = 10
-b = 20
-c = a + b
-print("The sum of a and b is:", c)
--- Module1-4/app.py
+++
@@ -1,7 +0,0 @@
-from Module5.mypackage.utils import clean_text
-
-mytext = clean_text("Hello, World!   Welcome to Python programming.")
-
-print(mytext)
-
-print("Hello, World!   Welcome to Python programming.")
